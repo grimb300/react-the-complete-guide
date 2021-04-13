7c49d540d236557d5e2d0e0acc162f8adc74ba28
Bob Grim <grimb300@hotmail.com>
2021-04-13 10:41:26 -0700

    handle uknown props in withClass HOC

--- rcg-lectures-retry-radium/src/components/Persons/Person/PersonClass.js
+++ rcg-lectures-retry-radium/src/components/Persons/Person/PersonClass.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
   render() {
@@ -7,12 +9,18 @@ class Person extends Component {
     console.log( '[Person.js] rendering... ', this.props.name );
   
     return (
-      <div className={ classes.Person } >
-        <p onClick={ this.props.click }>I'm { this.props.name } and I am { this.props.age } years old!</p>
-        <input type="text" onChange={ this.props.changed } value={ this.props.name } />
-      </div>
+      <Aux>
+        <p onClick={ this.props.click }>
+          I'm { this.props.name } and I am { this.props.age } years old!
+        </p>
+        <p key="i2">{this.props.children}</p>
+        <input
+          key="i3"
+          type="text"
+          onChange={ this.props.changed } value={ this.props.name } />
+      </Aux>
     );
   }
 }
 
-export default Person;
+export default withClass( Person, classes.Person );

7350867e8647c9b6411657c5cc436af5df0ba0dd
Bob Grim <grimb300@hotmail.com>
2021-04-13 15:12:29 -0700

    start burger builder app

---
+++ rcg-burger-builder/src/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={ styles.App } >
+      <h1>My Burger Builder App!!!</h1>
+    </div>
+  );
+}
+
+export default App;

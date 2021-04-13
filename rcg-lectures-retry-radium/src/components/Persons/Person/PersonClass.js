567f1aca83b13b136d71e3bd6ea61b21911735d9
Bob Grim <grimb300@hotmail.com>
2021-04-13 11:19:01 -0700

    add ref to Person

--- rcg-lectures-retry-radium/src/components/Persons/Person/PersonClass.js
+++ rcg-lectures-retry-radium/src/components/Persons/Person/PersonClass.js
@@ -5,6 +5,16 @@ import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
+  constructor( props ) {
+    super( props );
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
 
     console.log( '[Person.js] rendering... ', this.props.name );
@@ -17,6 +27,8 @@ class Person extends Component {
         <p key="i2">{this.props.children}</p>
         <input
           key="i3"
+          // ref={ ( inputEl ) => { this.inputElement = inputEl } }
+          ref={ this.inputElementRef }
           type="text"
           onChange={ this.props.changed }
           value={ this.props.name } />

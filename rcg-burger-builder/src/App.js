491b7d4c76f8d148eb6bf21676211f926549f093
Bob Grim <grimb300@hotmail.com>
2021-04-13 15:24:14 -0700

    clear out default App component and add Open Sans font

--- rcg-burger-builder/src/App.js
+++ rcg-burger-builder/src/App.js
@@ -1,11 +1,14 @@
+import React, { Component } from 'react';
 import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className={ styles.App } >
-      <h1>My Burger Builder App!!!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className={ styles.App } >
+        <h1>My Burger Builder App!!!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;

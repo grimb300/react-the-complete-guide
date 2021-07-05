bef45aa457a6552bf38fb8e01bb21f02f97be505
Bob Grim <grimb300@hotmail.com>
2021-07-05 15:17:54 -0700

    fixed input id problemm

--- new_course/react-complete-guide-section-11/src/components/UI/Input.js
+++ new_course/react-complete-guide-section-11/src/components/UI/Input.js
@@ -3,8 +3,8 @@ import classes from "./Input.module.css";
 const Input = (props) => {
   return (
     <div className={classes.input}>
-      <label htmlFor={props.id}>{props.label}</label>
-      <input type={props.type} {...props.input} />
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
     </div>
   );
 };

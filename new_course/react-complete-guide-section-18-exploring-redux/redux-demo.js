0d4fe94caf5d9a50f0a5d07ac455d896667d1e65
Bob Grim <grimb300@hotmail.com>
2021-07-13 14:55:14 -0700

    simple redux standalone example

---
+++ new_course/react-complete-guide-section-18-exploring-redux/redux-demo.js
@@ -0,0 +1,28 @@
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "INC") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DEC") {
+    return { counter: state.counter - 1 };
+  }
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "DEC" });
+store.dispatch({ type: "DEC" });
+store.dispatch({ type: "DEC" });

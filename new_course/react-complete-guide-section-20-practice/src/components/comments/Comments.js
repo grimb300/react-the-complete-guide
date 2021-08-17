cc9ea9f9409af0194072cb92124ba725ed6693d9
Bob Grim <grimb300@hotmail.com>
2021-08-17 16:11:35 -0700

    section 20

---
+++ new_course/react-complete-guide-section-20-practice/src/components/comments/Comments.js
@@ -0,0 +1,73 @@
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+
+import classes from "./Comments.module.css";
+import LoadingSpinner from "../ui/LoadingSpinner";
+import CommentsList from "./CommentsList";
+import NewCommentForm from "./NewCommentForm";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+
+const Comments = () => {
+  const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  const startAddCommentHandler = () => {
+    setIsAddingComment(true);
+  };
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner comments={loadedComments} />
+      </div>
+    );
+  }
+
+  if (status === "completed" && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (
+    status === "completed" &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className="centered">No comments, yet!</p>;
+  }
+
+  console.log({ status: status, loadedComments: loadedComments });
+
+  return (
+    <section className={classes.comments}>
+      <h2>User Comments</h2>
+      {!isAddingComment && (
+        <button className="btn" onClick={startAddCommentHandler}>
+          Add a Comment
+        </button>
+      )}
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={params.quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      )}
+      {comments}
+    </section>
+  );
+};
+
+export default Comments;
